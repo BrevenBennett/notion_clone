6b10d1187e44c5aa496679b90a9b66582207e0db
BrevenBennett <Breven.stars@gmail.com>
2023-11-09 14:49:27 -0600

    built toolbar component

--- app/(main)/_components/banner.tsx
+++ app/(main)/_components/banner.tsx
@@ -48,7 +48,7 @@ export const Banner = ({ documentId }: BannerProps) => {
         size="sm"
         onClick={onRestore}
         variant="outline"
-        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1px-2 h-auto font-normal"
+        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
       >
         Restore page
       </Button>
@@ -56,7 +56,7 @@ export const Banner = ({ documentId }: BannerProps) => {
         <Button
           size="sm"
           variant="outline"
-          className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1px-2 h-auto font-normal"
+          className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
         >
           Delete forever
         </Button>
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -2,12 +2,14 @@
 
 import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
+import { MenuIcon } from "lucide-react";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { MenuIcon } from "lucide-react";
+
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavBarProps {
   isCollapsed: boolean;
@@ -23,8 +25,11 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavBarProps) => {
 
   if (document === undefined) {
     return (
-        <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+        <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
             <Title.Skeleton />
+            <div className="flex items-center gap-x-2">
+              <Menu.Skeleton />
+            </div>
         </nav>
     )
   }
@@ -45,6 +50,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavBarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
             <Title initialData={document} />
+            <div className="flex items-center gap-x-2">
+              <Menu documentId={document._id} />
+            </div>
         </div>
       </nav>
       {document.isArchived && (
