3c2c723199a58122eb31e47506b31d63fbfd8b5c
BrevenBennett <Breven.stars@gmail.com>
2023-11-10 12:19:03 -0600

    building out cover image functionality

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -49,7 +49,7 @@ export const Title = ({ initialData }: TitleProps) => {
 
   return (
     <div className="flex items-center gap-x-1">
-      {!!initialData.icon && <p>initialData.icon</p>}
+      {!!initialData.icon && <p>{initialData.icon}</p>}
       {isEditing ? (
         <Input
           ref={inputRef}
